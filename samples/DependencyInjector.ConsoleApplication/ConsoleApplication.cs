f7073bf1ba0e3ac3fc89d1d75d1580549db7d6fc
chyla95 <chyla95@gmail.com>
2023-06-17 00:46:33 +0200

    Add sample console application

---
+++ samples/DependencyInjector.ConsoleApplication/ConsoleApplication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DependencyInjector.ConsoleApplication
+{
+	internal class ConsoleApplication
+	{
+		private readonly ClockService _clockService;
+
+		public ConsoleApplication(ClockService clockService)
+		{
+			_clockService = clockService;
+		}
+
+		public void Start()
+		{
+			DateTime time = _clockService.GetTime();
+			Console.WriteLine($"Time: {time}");
+		}
+	}
+
+	internal class ClockService
+	{
+		public DateTime GetTime() => DateTime.UtcNow;
+	}
+}
---
+++ samples/DependencyInjector.ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using DependencyInjector.ConsoleApplication;
+using DependencyInjector.Core;
+using DependencyInjector.Core.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+IDiBuilder builder = DiApplication.CreateBuilder();
+builder.Services.AddTransient<ClockService>();
+
+IDiApplication app = builder.Build(null);
+
+// Option I.
+//await app.StartAsync<ConsoleApplication>((consoleApplication, services, configuration) =>
+//{
+//	consoleApplication.Start();
+//});
+
+// Option II.
+//await app.StartAsync((services, configuration) =>
+//{
+//	ConsoleApplication worker = ActivatorUtilities.CreateInstance<ConsoleApplication>(services);
+//	worker.Start();
+//});
+
+await app.StopAsync();
